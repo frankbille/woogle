054606a4437e48533f47f448c06c19438b55cd81
Frank Bille <frankbille@apache.org>
2006-07-31 00:17:16 +0000

    Why AJAX? good question

--- src/main/java/wicket/contrib/woogle/pages/SearchPage.java
+++ src/main/java/wicket/contrib/woogle/pages/SearchPage.java
@@ -16,30 +16,20 @@ package wicket.contrib.woogle.pages;
 import java.util.Date;
 
 import wicket.PageParameters;
-import wicket.ajax.AbstractDefaultAjaxBehavior;
-import wicket.ajax.AjaxRequestTarget;
-import wicket.ajax.IAjaxCallDecorator;
-import wicket.ajax.IAjaxIndicatorAware;
-import wicket.ajax.calldecorator.AjaxCallDecorator;
-import wicket.ajax.form.AjaxFormSubmitBehavior;
-import wicket.behavior.SimpleAttributeModifier;
 import wicket.contrib.woogle.WoogleApplication;
 import wicket.contrib.woogle.WoogleSession;
 import wicket.contrib.woogle.components.SearchResult;
-import wicket.contrib.woogle.components.TopSearches;
 import wicket.contrib.woogle.domain.Search;
+import wicket.markup.html.WebComponent;
 import wicket.markup.html.form.Button;
 import wicket.markup.html.form.Form;
 import wicket.markup.html.form.TextField;
-import wicket.markup.html.image.Image;
 import wicket.model.CompoundPropertyModel;
 import wicket.model.Model;
 import wicket.util.string.Strings;
 
 public class SearchPage extends WoogleBasePage {
 	private static final long serialVersionUID = 1L;
-
-	private Image indicator;
 
 	public SearchPage(PageParameters parameters) {
 		// Check for search criterias in the parameters
@@ -55,7 +45,8 @@ public class SearchPage extends WoogleBasePage {
 
 		// Result place holder
 		if (searchString == null) {
-			add(new TopSearches("result").setOutputMarkupId(true));
+//			add(new TopSearches("result").setOutputMarkupId(true));
+			add(new WebComponent("result").setVisible(false));
 		} else {
 			// Save the search
 			String sessionId = WoogleSession.get().getId();
@@ -68,58 +59,33 @@ public class SearchPage extends WoogleBasePage {
 		}
 	}
 
-	private class SearchForm extends Form implements IAjaxIndicatorAware {
+	private class SearchForm extends Form {
 		private static final long serialVersionUID = 1L;
 
-		public SearchForm(String id, final Search search) {
+		private Search search;
+		
+		public SearchForm(String id, Search search) {
 			super(id, new CompoundPropertyModel(search));
 
-			add(new AjaxFormSubmitBehavior(this, "onsubmit") {
-				private static final long serialVersionUID = 1L;
-
-				@Override
-				protected void onSubmit(AjaxRequestTarget target) {
-					// Save the search
-					String sessionId = WoogleSession.get().getId();
-					search.setSessionId(sessionId);
-					search.setSearchTime(new Date());
-					WoogleApplication.get().getSearchDAO().save(search);
-
-					// Show the result
-					SearchResult result = new SearchResult("result", search
-							.getSearch());
-
-					SearchPage.this.replace(result);
-
-					target.addComponent(result);
-				}
-
-				@Override
-				protected IAjaxCallDecorator getAjaxCallDecorator() {
-					return new AjaxCallDecorator() {
-						private static final long serialVersionUID = 1L;
-
-						@Override
-						public CharSequence decorateScript(CharSequence script) {
-							return script + " return false";
-						}
-					};
-				}
-			});
-
+			this.search = search;
+			
 			add(new TextField("search"));
 			add(new Button("searchButton", new Model("Search")));
-
-			// Indicator
-			indicator = new Image("indicator",
-					AbstractDefaultAjaxBehavior.INDICATOR);
-			indicator.setOutputMarkupId(true);
-			indicator.add(new SimpleAttributeModifier("style", "display:none"));
-			add(indicator);
 		}
 
-		public String getAjaxIndicatorMarkupId() {
-			return indicator.getMarkupId();
+		@Override
+		protected void onSubmit() {			
+			// Save the search
+			String sessionId = WoogleSession.get().getId();
+			search.setSessionId(sessionId);
+			search.setSearchTime(new Date());
+			WoogleApplication.get().getSearchDAO().save(search);
+			
+			// Redirect to search result page.
+			// We use page parameters to get a bookmarkable url
+			PageParameters parameters = new PageParameters();
+			parameters.add("search", search.getSearch());
+			setResponsePage(SearchPage.class, parameters);
 		}
 	}
 }
